0207fa28e3ecea564969c9d1352c800ffc976242
cw-nikhilnaik <155508579+cw-nikhilnaik@users.noreply.github.com>
2024-05-22 18:38:19 +0530

    Created basic layout of recycler view

---
+++ app/src/main/java/com/example/used/RecylerContentAdapter.java
@@ -0,0 +1,24 @@
+package com.example.used;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class RecylerContentAdapter extends RecyclerView.Adapter<RecylerContentAdapter.ViewHolder> {
+    @NonNull
+    @Override
+    public RecylerContentAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecylerContentAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
